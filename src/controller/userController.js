31caab1ae62a7cab9c4d36ba8963ec71c85abc50
Talha <talhadar2011@gmail.com>
2026-02-12 13:27:05 +0100

    controller added

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,11 +1,18 @@
 import userModel from "../models/userModel.js";
 
-exports.createUser= async(res,req)=>{
-    try{
-        const{name,email,password}=req.body;
-        const user=await User
+exports.createUser = async (res, req) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await userModel.findone({ email })
+        if (user) {
+            res.status(400).json({ message: "User with this email already exisit." })
+        }
+        else
+        {
+            
+        }
     }
-    catch(error){
-        res.status(400).json({message:error.message})
+    catch (error) {
+        res.status(400).json({ message: error.message })
     }
 }
